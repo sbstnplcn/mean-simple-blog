993ffd5cd3f2ed7a99d714226c68f50a899913ec
Sebastien <sbstnplcn@gmail.com>
2016-11-18 17:03:52 +0100

    start API for manage users

--- public/js/components/user.js
+++ public/js/components/user.js
@@ -11,6 +11,46 @@
                         this.users = res.data
                     })
 
+                    //// editMode ////
+                    //
+                    let previous = {}
+
+                    // edit
+                    this.edit = (user, position) => {
+                        if (this.editMode) {
+                            usersService.edit(user).then((res) => {
+                                this.editMode = false
+                            })
+                        } else {
+                            previous[user.position] = angular.copy(user)
+                            this.editMode = true
+                        }
+                    }
+
+                    // cancel
+                    this.cancel = (user) => {
+                        user = previous[user.position]
+                        this.users[this.userView] = user
+                        this.editMode = false
+
+                    }
+
+                    // delete
+                    this.delete = (user, position) => {
+                        usersService.delete(user).then((res) => {
+                            this.users.splice(user.position, 1)
+                        })
+                    }
+
+                    // add Article
+                    this.add = () => {
+                        usersService.add(this.newUser).then((res) => {
+                            this.newUser
+                            this.users.push(this.newUser)
+                            this.addNewUser = null
+                        })
+                    }
+
                 }
             })
         }]
